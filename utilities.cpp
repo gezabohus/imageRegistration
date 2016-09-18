f42421f70d6b073ef4c484a65c8243d92fcc3058
Geza Bohus <gbohus@yahoo.com>
2016-09-18 20:23:16 +0200

    Minor changes, adding/deleting test images

--- utilities.cpp
+++ utilities.cpp
@@ -31,12 +31,12 @@ std::string transformString(const std::string & s_, const char * tr_)
 
 double absAverageDiff(const double & t0, const double & t1)
 {
-  return abs(t0 - t1);
+  return std::abs(t0 - t1);
 }
   
 double absAverageDiff(const drgb & t0, const drgb & t1)
 {
-  return abs(t0.r - t1.r);
+  return std::abs(t0.r - t1.r);
 }
   
 } //namespace imageRegistration
